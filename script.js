f6361efb7b25d6fc9cedbdbcd27dcfa7b34211f2
Daniel <daniel105k5@gmail.com>
2023-09-05 16:22:49 +0200

    insertion sort

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const graph = document.querySelector("#graph");
 const shuffle = document.querySelector("#shuffle");
 const start = document.querySelector("#start");
+const bubble = document.querySelector("#bubble");
+const insertion = document.querySelector("#insertion");
+const selector = document.querySelectorAll("#selector > button");
 
 let arr = [];
 let isSorting = false;
 let isSorted = true;
 let mobile = false;
-
+let sortType = bubbleSort;
 
 for (let i = 1; i <= 50; i++) {
     arr.push(i * 10);
@@ -55,20 +58,20 @@ function wait(ms) {
     });
   }
 
-async function sort() {
+async function bubbleSort() {
     if (isSorting || isSorted) return;
     isSorting = true;
     for (let i = 0; i < arr.length; i++) {
 
         for (let j = 0; j < arr.length - i - 1; j++) {
-            let graphElemnts = graph.querySelectorAll("*")
+            let graphElemnts = graph.querySelectorAll("*");
             graphElemnts[j].style.backgroundColor = "green";
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
-            await wait(1)
+            await wait(10)
             updateGraph() 
             
         }     
@@ -78,8 +81,46 @@ async function sort() {
     isSorted = true;
 }
 
-shuffle.addEventListener("click", shuffleArray);
-start.addEventListener("click", sort);
+async function insertionSort () {
+    if (isSorting || isSorted) return;
+    isSorting = true;
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
 
+        let graphElemnts = graph.querySelectorAll("*");
+        graphElemnts[i].style.backgroundColor = "green";
+
+        while (j >= 0 && current < arr[j]) {
+            graphElemnts[j].style.backgroundColor = "red";
+            await wait(20)
+            graphElemnts[j].style.backgroundColor = "var(--color-5)";
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+        await wait(50);
+        updateGraph();
+    }
+    isSorting = false;
+    isSorted = true;
+}
+
+shuffle.addEventListener("click", shuffleArray);
+start.addEventListener("click", () => sortType());
+
+bubble.addEventListener("click", () => {
+    if (isSorting) return;
+    sortType = bubbleSort;
+    selector.forEach(btn => btn.classList.remove("selected"))
+    bubble.classList.add("selected")
+})
+
+insertion.addEventListener("click", () => {
+    if (isSorting) return;
+    sortType = insertionSort;
+    selector.forEach(btn => btn.classList.remove("selected"))
+    insertion.classList.add("selected")
+})
 
   
