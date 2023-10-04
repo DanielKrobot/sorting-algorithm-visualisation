0597e80ae752c4c2cf84304c8c68fc13bac1bd77
Daniel <daniel105k5@gmail.com>
2023-10-04 16:51:40 +0200

    merge sort

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const start = document.querySelector("#start");
 const bubble = document.querySelector("#bubble");
 const insertion = document.querySelector("#insertion");
 const selection = document.querySelector("#selection");
+const merge = document.querySelector("#merge");
 const selector = document.querySelectorAll("#selector > button");
 
 let arr = [];
@@ -141,6 +142,51 @@ async function selectionSort() {
     isSorted = true;
 }
 
+async function mergeSort(array) {
+    array = await array;
+    const mid = array.length / 2;
+    if (array.length < 2) return array;
+
+    const left = array.splice(0, mid);
+    const first = await mergeSort(left);
+    const second = await mergeSort(array)
+    const merged = await merging(first, second)
+    await wait(100)
+    return merged;
+}
+
+async function merging(left, right) {
+    let graphElemnts = graph.querySelectorAll("*");
+    left = await left
+    right = await right
+    const concatArr = left.concat(right)
+    for (const element of concatArr){
+        
+        graphElemnts[arr.indexOf(element)].style.backgroundColor = "green";
+        await wait(50);
+    }
+    await wait(50)
+    updateGraph()
+    
+    let array = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            array.push(left.shift());
+        } else {
+            array.push(right.shift());
+        }
+    }
+
+    array = [...array, ...left, ...right];
+    let min = arr.indexOf(concatArr[0]);
+    for (let i = 0; i < array.length; i ++) {
+        arr[min + i] = array[i]; 
+    }
+    updateGraph();
+    return [...array]; 
+
+}
+
 shuffle.addEventListener("click", shuffleArray);
 start.addEventListener("click", () => sortType());
 
@@ -164,3 +210,19 @@ selection.addEventListener("click", () => {
     selector.forEach(btn => btn.classList.remove("selected"))
     selection.classList.add("selected")
 })  
+
+merge.addEventListener("click", () => {
+    if (isSorting) return;
+    sortType = () => {
+        new Promise((resolve) => {
+            resolve(mergeSort([...arr]))
+        }).then((result) => {
+            arr = result;
+            updateGraph()
+        })
+        isSorting = false;
+        isSorted = true;
+    };
+    selector.forEach(btn => btn.classList.remove("selected"))
+    merge.classList.add("selected")
+})
